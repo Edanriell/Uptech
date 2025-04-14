9b70963f791d5b4bdef7acc1c53e0aaeb27ccb71
Edanriell <Edanriell@gmail.com>
2025-04-14 21:58:46 +0300

    feature(newsletter-optimization): optimized newsletter form

--- Client/uptech/src/widgets/newsletter/ui/newsletter-submit-button.tsx
+++ Client/uptech/src/widgets/newsletter/ui/newsletter-submit-button.tsx
@@ -1,4 +1,4 @@
-import { type FC } from "react";
+import { memo } from "react";
 import { AnimatePresence, motion } from "motion/react";
 
 import { Button } from "@shared/ui/button/ui";
@@ -11,135 +11,136 @@ type NewsletterSubmitButtonProps = {
 	windowWidth: number;
 };
 
-export const NewsletterSubmitButton: FC<NewsletterSubmitButtonProps> = ({
-	formState,
-	windowWidth
-}) => {
-	const submitButtonAnimationVariants = {
-		idle: {
-			width: windowWidth <= 990 ? "100%" : "149rem"
-		},
-		loading: {
-			width: windowWidth <= 990 ? "100%" : "96rem"
-		},
-		success: {
-			width: windowWidth <= 990 ? "100%" : "188rem"
-		},
-		failure: {
-			width: windowWidth <= 990 ? "100%" : "170rem"
-		}
-	};
+export const NewsletterSubmitButton = memo(
+	({ formState, windowWidth }: NewsletterSubmitButtonProps) => {
+		const submitButtonAnimationVariants = {
+			idle: {
+				width: windowWidth <= 990 ? "100%" : "149rem"
+			},
+			loading: {
+				width: windowWidth <= 990 ? "100%" : "96rem"
+			},
+			success: {
+				width: windowWidth <= 990 ? "100%" : "188rem"
+			},
+			failure: {
+				width: windowWidth <= 990 ? "100%" : "170rem"
+			}
+		};
 
-	const renderButtonStaticLayerContent = (state: typeof formState) => {
-		switch (state) {
-			case NewsletterFormState.IDLE:
-				return (
-					<span className="drop-shadow-lg flex w-full justify-center items-start text-white-50 font-medium">
-						Subscribe
-					</span>
-				);
-			case NewsletterFormState.LOADING:
-				return <Spinner width={32} height={32} />;
-			case NewsletterFormState.SUCCESS:
-				return (
-					<span className="drop-shadow-lg flex w-full justify-center items-start text-white-50 font-medium gap-x-[6rem]">
-						Subscribed
-						<span>🎉</span>
-					</span>
-				);
-			case NewsletterFormState.FAILURE:
-				return (
-					<span className="drop-shadow-lg flex w-full justify-center items-start text-white-50 font-medium gap-x-[6rem]">
-						Try again
-						<span>❌</span>
-					</span>
-				);
-			default:
-				return null;
-		}
-	};
+		const renderButtonStaticLayerContent = (state: typeof formState) => {
+			switch (state) {
+				case NewsletterFormState.IDLE:
+					return (
+						<span className="drop-shadow-lg flex w-full justify-center items-start text-white-50 font-medium">
+							Subscribe
+						</span>
+					);
+				case NewsletterFormState.LOADING:
+					return <Spinner width={32} height={32} />;
+				case NewsletterFormState.SUCCESS:
+					return (
+						<span className="drop-shadow-lg flex w-full justify-center items-start text-white-50 font-medium gap-x-[6rem]">
+							Subscribed
+							<span>🎉</span>
+						</span>
+					);
+				case NewsletterFormState.FAILURE:
+					return (
+						<span className="drop-shadow-lg flex w-full justify-center items-start text-white-50 font-medium gap-x-[6rem]">
+							Try again
+							<span>❌</span>
+						</span>
+					);
+				default:
+					return null;
+			}
+		};
 
-	const renderButtonDynamicLayerContent = (state: typeof formState) => {
-		switch (state) {
-			case NewsletterFormState.IDLE:
-				return (
-					<span className="drop-shadow-lg flex w-full justify-center items-start text-shark-950 font-semibold">
-						Subscribe
-					</span>
-				);
-			case NewsletterFormState.LOADING:
-				return (
-					<Spinner
-						width={32}
-						height={32}
-						primaryColor="rgba(0,0,0, 0.25)"
-						secondaryColor="rgba(0,0,0, 1)"
-					/>
-				);
-			case NewsletterFormState.SUCCESS:
-				return (
-					<span className="drop-shadow-lg flex w-full justify-center items-start text-shark-950 font-semibold gap-x-[6rem]">
-						Subscribed
-						<span>🎉</span>
-					</span>
-				);
-			case NewsletterFormState.FAILURE:
-				return (
-					<span className="drop-shadow-lg flex w-full justify-center items-start text-shark-950 font-semibold gap-x-[6rem]">
-						Try again
-						<span>❌</span>
-					</span>
-				);
-			default:
-				return null;
-		}
-	};
+		const renderButtonDynamicLayerContent = (state: typeof formState) => {
+			switch (state) {
+				case NewsletterFormState.IDLE:
+					return (
+						<span className="drop-shadow-lg flex w-full justify-center items-start text-shark-950 font-semibold">
+							Subscribe
+						</span>
+					);
+				case NewsletterFormState.LOADING:
+					return (
+						<Spinner
+							width={32}
+							height={32}
+							primaryColor="rgba(0,0,0, 0.25)"
+							secondaryColor="rgba(0,0,0, 1)"
+						/>
+					);
+				case NewsletterFormState.SUCCESS:
+					return (
+						<span className="drop-shadow-lg flex w-full justify-center items-start text-shark-950 font-semibold gap-x-[6rem]">
+							Subscribed
+							<span>🎉</span>
+						</span>
+					);
+				case NewsletterFormState.FAILURE:
+					return (
+						<span className="drop-shadow-lg flex w-full justify-center items-start text-shark-950 font-semibold gap-x-[6rem]">
+							Try again
+							<span>❌</span>
+						</span>
+					);
+				default:
+					return null;
+			}
+		};
 
-	return (
-		<Button.Provider>
-			<Button
-				className="tablet:w-[149rem] shrink-0 grow-0"
-				transitionOptions={{ type: "spring", duration: 0.5, bounce: 0 }}
-				type="submit"
-				variants={submitButtonAnimationVariants}
-				animate={formState}
-				initial={false}
-			>
-				<Button.StaticLayer className="flex items-center justify-center z-10 pointer-events-none">
-					<AnimatePresence mode="popLayout" initial={false}>
-						<motion.span
-							transition={{
-								type: "spring",
-								duration: 1,
-								bounce: 0
-							}}
-							initial={{ opacity: 0, y: -50, filter: "blur(3.8rem)" }}
-							animate={{ opacity: 1, y: 0, filter: "blur(0rem)" }}
-							exit={{ opacity: 0, y: 50, filter: "blur(3.8rem)" }}
-							key={formState}
-						>
-							{renderButtonStaticLayerContent(formState)}
-						</motion.span>
-					</AnimatePresence>
-				</Button.StaticLayer>
-				<Button.DynamicLayer className="flex items-center justify-center z-20 pointer-events-none">
-					<AnimatePresence mode="popLayout" initial={false}>
-						<motion.span
-							transition={{
-								type: "spring",
-								duration: 1,
-								bounce: 0
-							}}
-							initial={{ opacity: 0, y: -50, filter: "blur(3.8rem)" }}
-							animate={{ opacity: 1, y: 0, filter: "blur(0rem)" }}
-							exit={{ opacity: 0, y: 50, filter: "blur(3.8rem)" }}
-							key={formState}
-						>
-							{renderButtonDynamicLayerContent(formState)}
-						</motion.span>
-					</AnimatePresence>
-				</Button.DynamicLayer>
-			</Button>
-		</Button.Provider>
-	);
-};
+		return (
+			<Button.Provider>
+				<Button
+					className="tablet:w-[149rem] shrink-0 grow-0"
+					transitionOptions={{ type: "spring", duration: 0.5, bounce: 0 }}
+					type="submit"
+					variants={submitButtonAnimationVariants}
+					animate={formState}
+					initial={false}
+				>
+					<Button.StaticLayer className="flex items-center justify-center z-10 pointer-events-none">
+						<AnimatePresence mode="popLayout" initial={false}>
+							<motion.span
+								transition={{
+									type: "spring",
+									duration: 1,
+									bounce: 0
+								}}
+								initial={{ opacity: 0, y: -50, filter: "blur(3.8rem)" }}
+								animate={{ opacity: 1, y: 0, filter: "blur(0rem)" }}
+								exit={{ opacity: 0, y: 50, filter: "blur(3.8rem)" }}
+								key={formState}
+							>
+								{renderButtonStaticLayerContent(formState)}
+							</motion.span>
+						</AnimatePresence>
+					</Button.StaticLayer>
+					<Button.DynamicLayer className="flex items-center justify-center z-20 pointer-events-none">
+						<AnimatePresence mode="popLayout" initial={false}>
+							<motion.span
+								transition={{
+									type: "spring",
+									duration: 1,
+									bounce: 0
+								}}
+								initial={{ opacity: 0, y: -50, filter: "blur(3.8rem)" }}
+								animate={{ opacity: 1, y: 0, filter: "blur(0rem)" }}
+								exit={{ opacity: 0, y: 50, filter: "blur(3.8rem)" }}
+								key={formState}
+							>
+								{renderButtonDynamicLayerContent(formState)}
+							</motion.span>
+						</AnimatePresence>
+					</Button.DynamicLayer>
+				</Button>
+			</Button.Provider>
+		);
+	}
+);
+
+NewsletterSubmitButton.displayName = "NewsletterSubmitButton";
--- Client/uptech/src/widgets/newsletter/ui/newsletter.tsx
+++ Client/uptech/src/widgets/newsletter/ui/newsletter.tsx
@@ -1,58 +1,19 @@
 "use client";
 
 import { type FC } from "react";
-import { AnimatePresence, motion } from "motion/react";
 
-import { useNewsletterForm } from "../lib/hooks";
-
-import { NewsletterSubmitButton } from "./newsletter-submit-button";
+import { NewsletterForm } from "./newsletter-form";
 
 export const Newsletter: FC = () => {
-	const { formState, windowWidth, register, handleSubmit, errors, handleNewsletterFormSubmit } =
-		useNewsletterForm();
-
 	return (
 		<div className="tablet:basis-[43%] desktop:mr-[unset] desktop:basis-[477rem]">
 			<h2 className="text-[40rem] font-medium leading-[125%] text-white-50 mb-[24rem] opacity-[0.9]">
 				Stay Updated on Latest Product Releases
 			</h2>
-			<form
-				onSubmit={handleSubmit(handleNewsletterFormSubmit)}
-				className="flex flex-col gap-y-[8rem] mb-[40rem] tablet:flex-row tablet:gap-x-[8rem] tablet:mb-[unset]"
-			>
-				<motion.div layout className="relative w-full tablet:basis-[320rem]">
-					<label className="visually-hidden" htmlFor="email">
-						Enter your email
-					</label>
-					<input
-						{...register("email")}
-						id="email"
-						name="email"
-						placeholder="Enter your email"
-						type="email"
-						className="border-solid border-[1rem] border-white-900 rounded-[46rem] px-[24rem] py-[14rem] max-h-[52rem] bg-white-950 text-[16rem] font-light leading-[150%] text-white-50 placeholder:text-[#FFFFFFCC] w-full"
-					/>
-					<AnimatePresence>
-						{errors.email && (
-							<motion.p
-								initial={{ opacity: 0, x: 20, filter: "blur(4rem)" }}
-								animate={{ opacity: 1, x: 0, filter: "blur(0rem)" }}
-								exit={{ opacity: 0, x: 20, filter: "blur(4rem)" }}
-								className="text-[16rem] font-medium leading-[150%] text-alizarin-crimson-500 absolute top-[-30rem] tablet:top-[unset] tablet:bottom-[-30rem] left-[24rem]"
-							>
-								{errors.email?.message}
-							</motion.p>
-						)}
-					</AnimatePresence>
-				</motion.div>
-				<NewsletterSubmitButton windowWidth={windowWidth} formState={formState} />
-			</form>
+			<NewsletterForm />
 		</div>
 	);
 };
-
-// TODO
-// Decompose input if it is not unique across website
 
 // TODO
 // Also drawer exit animation must use radix
